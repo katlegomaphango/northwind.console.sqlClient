d5d65422b598201e1940f2f7b41b048f4d00de03
katlegomaphango <mphangokatlego08@gmail.com>
2023-09-18 23:49:48 +0200

    sql builder

--- northwind.console.sqlClient.App/northwind.console.sqlClient/Program.cs
+++ northwind.console.sqlClient.App/northwind.console.sqlClient/Program.cs
@@ -1,7 +1,13 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
+﻿using Microsoft.Data.SqlClient;
+
+SqlConnectionStringBuilder builder = new();
+
+builder.InitialCatalog = "northWind";
+builder.MultipleActiveResultSets = true;
+builder.Encrypt = true;
+builder.TrustServerCertificate = true;
+builder.ConnectTimeout = 10;
+
+
+
+
