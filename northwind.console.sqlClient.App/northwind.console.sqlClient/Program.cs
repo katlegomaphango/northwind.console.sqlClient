de75c86f1ea5c15978e3436a419d3111e5960528
katlegomaphango <mphangokatlego08@gmail.com>
2023-09-18 23:50:10 +0200

    platform menu

--- northwind.console.sqlClient.App/northwind.console.sqlClient/Program.cs
+++ northwind.console.sqlClient.App/northwind.console.sqlClient/Program.cs
@@ -8,6 +8,15 @@ builder.Encrypt = true;
 builder.TrustServerCertificate = true;
 builder.ConnectTimeout = 10;
 
+Console.WriteLine("Connect to: ");
+Console.WriteLine("\t1. - SQL Server on local machine");
+Console.WriteLine("\t2. - Azure SQL Database");
+Console.WriteLine("\t3. - Azure SQL Edge");
+Console.WriteLine();
+Console.Write("\tPress a key: ");
 
+ConsoleKey key = Console.ReadKey().Key;
+Console.WriteLine();
+Console.WriteLine();
 
 
