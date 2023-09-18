f80a38fa50d7a22b4a360d9a466f12e1a1b0fc16
katlegomaphango <mphangokatlego08@gmail.com>
2023-09-18 23:32:27 +0200

    state change

---
+++ northwind.console.sqlClient.App/northwind.console.sqlClient/Program.EventHandlers.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace northwind.console.sqlClient;
+
+internal class Program
+{
+    static void Connection_StateChange(Object sender, StateChangeEventArgs e)
+    {
+        ConsoleColor prevColor = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+        Console.WriteLine($"State change from {e.OriginalState} to {e.CurrentState}");
+
+        Console.ForegroundColor = prevColor;
+    }
+}
